664912ee8ec80e7d11a260355e18f5ce4af219f7
Alex Waese-Perlman <alexwaeseperlman@gmail.com>
2022-06-15 23:18:01 -0400

    Fix module resolution in Jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./tsconfig')
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testURL: 'http://bar.utoronto.ca/eplant',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: __dirname,
+  }),
 }
