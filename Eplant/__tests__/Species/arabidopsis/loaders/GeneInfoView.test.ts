0d1b71deb3bb39a297d653aaabfddcabbdbf9444
Alex Waese-Perlman <alexwaeseperlman@gmail.com>
2022-06-28 02:57:24 -0400

    Fix failing tests

--- Eplant/__tests__/Species/arabidopsis/loaders/GeneInfoView.test.ts
+++ Eplant/__tests__/Species/arabidopsis/loaders/GeneInfoView.test.ts
@@ -2,12 +2,16 @@ import loadData from '@eplant/Species/arabidopsis/loaders/GeneInfoView'
 import GeneticElement from '@eplant/GeneticElement'
 import { GeneInfoViewData } from '@eplant/views/GeneInfoView'
 import arabidopsis from '@eplant/Species/arabidopsis'
+import ArabidopsisGeneInfoView from '@eplant/Species/arabidopsis/loaders/GeneInfoView'
 
 describe('load data', () => {
   it('should correctly load data for AT3G24650', async () => {
     const gene = await arabidopsis.api.searchGene('AT3G24650')
     if (!gene) throw new Error("Couldn't find gene")
-    const data = await loadData(gene, (loading: number) => {})
+    const data = await ArabidopsisGeneInfoView.loadData(
+      gene,
+      (loading: number) => {}
+    )
     //console.log(
     JSON.stringify({
       geneticElement: gene,
