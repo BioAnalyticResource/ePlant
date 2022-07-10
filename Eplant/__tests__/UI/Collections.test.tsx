af1e7a964cb4a7e7fda103b1f00dd4f72a9bbcf5
Alex Waese-Perlman <alexwaeseperlman@gmail.com>
2022-07-10 11:51:30 +0300

    FIx some tests

--- Eplant/__tests__/UI/Collections.test.tsx
+++ Eplant/__tests__/UI/Collections.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { prettyDOM, render } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import * as React from 'react'
 import { LeftNav } from '@eplant/UI/LeftNav'
@@ -34,7 +34,7 @@ describe('Collection', () => {
         onRemove={onRemove}
       ></Collection>
     )
-    const parent = await s.findByTestId('collection')
+    const parent = await s.findByText(name)
     return { onNameChange, setOpen, deleteGene, onRemove, s, parent }
   }
 
@@ -58,7 +58,7 @@ describe('Collection', () => {
       name: 'name',
       open: false,
     })
-    await userEvent.click(parent.children[0])
+    await userEvent.click(parent)
     expect(setOpen).toHaveBeenCalledTimes(1)
   })
   it('should open when clicked while open', async () => {
@@ -66,7 +66,7 @@ describe('Collection', () => {
       name: 'name',
       open: true,
     })
-    await userEvent.click(parent.children[0])
+    await userEvent.click(parent)
     expect(setOpen).toHaveBeenCalledTimes(1)
   })
 })
