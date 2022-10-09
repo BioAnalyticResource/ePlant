4f5ce14f94aade893e705a5188b3f8a9919f6ae1
Gabriel Dinner-David <gabydinnerdavid@gmail.com>
2022-10-09 14:47:38 -0400

    fix formating in config.ts

--- Eplant/config.ts
+++ Eplant/config.ts
@@ -2,19 +2,19 @@ import React from 'react'
 import { type View } from './View'
 
 export type EplantConfig = {
-    readonly genericViews: View[]
-      readonly userViews: View[]
-        readonly views: View[]
-          readonly tabHeight: number
-            readonly rootPath: string
+  readonly genericViews: View[]
+  readonly userViews: View[]
+  readonly views: View[]
+  readonly tabHeight: number
+  readonly rootPath: string
 }
 
 export const Config = React.createContext<EplantConfig>({
-    genericViews: [],
-      userViews: [],
-        views: [],
-          tabHeight: 48,
-            rootPath: ''
+  genericViews: [],
+  userViews: [],
+  views: [],
+  tabHeight: 48,
+  rootPath: ''
 })
 
 export const useConfig = () => React.useContext(Config)
