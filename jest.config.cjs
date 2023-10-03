de8a5ddb59c55b2f374e91d481f6a14ab9e5d6e4
Zoe Daniels <7177924+zkdan@users.noreply.github.com>
2023-10-03 17:47:26 -0400

    squaring my package file with the one in Mike's PR, changing the jest config to allow the tests to pass

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable-next-line @typescript-eslint/no-var-requires*/
 const path = require('path')
 
 module.exports = {
