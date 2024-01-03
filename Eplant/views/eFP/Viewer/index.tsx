c596622c5c4b8742da1199980bb41bb35700171b
yukthiw <yukthiw61@gmail.com>
2024-01-02 20:39:50 -0500

    Updated Link tag

--- Eplant/views/eFP/Viewer/index.tsx
+++ Eplant/views/eFP/Viewer/index.tsx
@@ -2,7 +2,7 @@ import GeneticElement from '@eplant/GeneticElement'
 import PanZoom from '@eplant/util/PanZoom'
 import { View, ViewProps } from '@eplant/View'
 import { ViewDataError } from '@eplant/View/viewData'
-import { Box, MenuItem, Tooltip, Typography } from '@mui/material'
+import { Box, Link, MenuItem, Tooltip, Typography } from '@mui/material'
 import React, { startTransition, useEffect, useMemo, useState } from 'react'
 import EFP from '..'
 import EFPPreview from '../EFPPreview'
@@ -495,7 +495,7 @@ export default class EFPViewer
               <></>
           }
           <br></br> This image was generated with the {viewID} at bar.utoronto.ca/eplant by Waese et al. 2017.
-          <a target="_blank" rel="license oopener noreferrer " href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" src="https://i.creativecommons.org/l/by/4.0/80x15.png" title="The ePlant output for your gene of interest is available under a Creative Commons Attribution 4.0 International License and may be freely used in publications etc." /></a>
+          <Link href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" src="https://i.creativecommons.org/l/by/4.0/80x15.png" title="The ePlant output for your gene of interest is available under a Creative Commons Attribution 4.0 International License and may be freely used in publications etc." /></Link>
         </div>
       )
     } else {
