230e1f4152caa70d6d583a12f510f367a2e0456b
Zoe Daniels <7177924+zkdan@users.noreply.github.com>
2024-01-18 16:46:29 -0500

    changing css to style in account for theme file

--- Eplant/main.tsx
+++ Eplant/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'jotai'
 import { useDarkMode } from '@eplant/state'
 import { CssBaseline, ThemeProvider } from '@mui/material'
-import { dark, light } from './css/theme'
+import { dark, light } from './style/theme'
 
 import FallbackView from './views/FallbackView'
 import GeneInfoView from './views/GeneInfoView'
