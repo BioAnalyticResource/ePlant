2a7fb20a33a9b5278f90f9f1131d52d318af8120
Yukthi <yukthi.wickramarachchi@mail.utoronto.ca>
2024-01-23 21:40:53 -0500

    Updated eslint and tsconfig, changed double quotes to single

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,5 +23,6 @@ module.exports = {
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
+    'react/react-in-jsx-scope': 'off',
   },
 }
