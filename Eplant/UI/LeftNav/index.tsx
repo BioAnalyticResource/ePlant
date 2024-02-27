7742d6fe3174cead1ee74dfb47c31aa814858805
Apiraam Selvabaskaran <apiraam96@gmail.com>
2024-02-27 12:08:41 -0500

    style: changed color of collapsible side panel arrow to match theme

--- Eplant/UI/LeftNav/index.tsx
+++ Eplant/UI/LeftNav/index.tsx
@@ -61,7 +61,7 @@ export function LeftNav(props: {
         <div style={
           !collapse ? {cursor: 'pointer', height: '25px', width: `${tabWidth}`, marginRight: '-20px',backgroundColor: `${theme.palette.primary.main}`, borderRadius: '20px 0px 0px 20px', transform: 'translateX(+20%)', transition: 'all 1s ease-out'} : {cursor: 'pointer', height: '25px', width: `${tabWidth}`, marginRight: '-37px', backgroundColor: `${theme.palette.primary.main}`, borderRadius: '20px 0px 0px 20px', transform: 'translateX(-30%)', transition: 'all 1s ease-out' }
         } onClick={() => toggleCollapse()} onMouseEnter={() => setTabWidth('40px')} onMouseLeave={() => setTabWidth('30px')}>
-          {collapse ? <ArrowRight sx={darkMode ? {color: 'primary'} : {color: 'secondary'}}/> : <ArrowLeft/>}
+          {collapse ? <ArrowRight sx={darkMode ? {color: '#181c18'} : {color: '#f0f7f0'}}/> : <ArrowLeft sx={darkMode ? {color: '#181c18'} : {color: '#f0f7f0'}}/>}
         </div>
       </div>
       {!collapse && <><SearchGroup
