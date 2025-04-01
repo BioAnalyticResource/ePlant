a9211462222ff81397c4fe89d7047f008cfb3cfc
k.jamal <kabsschmals@outlook.com>
2025-04-01 16:42:28 -0400

    Changed cell efp icon export to use the same structure as the others

--- Eplant/views/NavigatorViewer/Icons/Nav_CellEFPIcon.tsx
+++ Eplant/views/NavigatorViewer/Icons/Nav_CellEFPIcon.tsx
@@ -1,5 +1,6 @@
 import { styled } from '@mui/material'
-export default styled(function CellEFPIcon(props) {
+
+export default styled((props) => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
